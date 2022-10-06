af8074154531028895bb9d23e1cdb6b054f26116
arrat <arrat@10.166.49.35>
2022-10-06 15:53:40 +0200

    commit

---
+++ ProyectoProg3/src/Habilidades.java
@@ -0,0 +1,30 @@
+
+public enum Habilidades {
+
+//	asignar a cada trabajador una habilidad
+//	trabajadores actuales
+//	trabajadores necesarios
+//	RECURSIVIDAD : Asignsr trabajador a tiendas.
+	
+	
+	//hilo conrecursiva.
+//	
+//	admin : añadirun boton que te lleve a otra ventana.
+//	trabajador
+//	
+//	(misma ventana)
+	
+	//interfaz:
+	
+//	estadisticas: ganancias, coste de cada trabajador.
+	
+	
+//libreria SQL (base de datos).
+	
+	//ventana iniciar sesion y registro compartidas.
+
+	
+	codigo de uso generico para toda la clase
+	
+	
+}
