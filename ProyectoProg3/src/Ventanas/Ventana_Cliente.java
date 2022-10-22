ee500237b6cb1eb59ce53d64686966c1c36b289b
msang <msang@LAPTOP-3CIOERQQ>
2022-10-22 10:32:18 +0200

    Ventana cliente

---
+++ ProyectoProg3/src/Ventanas/Ventana_Cliente.java
@@ -0,0 +1,105 @@
+package Ventanas;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextField;
+
+import Clasesprincipales.TipoProducto;
+import Clasesprincipales.Talla;
+
+public class Ventana_Cliente extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel arriba;
+	private JPanel centro;
+	private JPanel centro_izda;
+	private JPanel centro_dcha;
+	private JPanel abajo;
+	
+	private JLabel cliente;
+	private JLabel tipo;
+	private JLabel color;
+	private JLabel precio;
+	private JLabel talla;
+	
+	private JTextField c;
+	private JTextField informacion;
+	
+	private JScrollBar preciobarra;
+	
+	private JComboBox<TipoProducto> tipos;
+	private JComboBox<Talla> tallas;
+	
+	private JButton buscar;
+	private JButton anyadir;
+		
+	
+	public Ventana_Cliente() {
+		inicializarVentana();
+	}
+
+
+	private void inicializarVentana() {
+		this.setSize(400, 400);
+		this.setLayout(new GridLayout(3,1));
+		
+		arriba = new JPanel();
+		centro = new JPanel();
+		centro.setLayout(new GridLayout(2,1));
+		centro_izda = new JPanel();
+		centro_izda.setLayout(new GridLayout(2,1));
+		centro_dcha = new JPanel();
+		abajo = new JPanel();
+		
+		cliente = new JLabel("CLIENTE");
+		tipo = new JLabel("Tipo:");
+		color = new JLabel("Color:");
+		precio = new JLabel ("Precio:");
+		talla = new JLabel("Talla:");
+		
+		preciobarra = new JScrollBar();
+		
+		c = new JTextField("", 16);
+		informacion = new JTextField();
+		informacion.setEditable(false);
+		
+		tipos = new JComboBox<TipoProducto>();
+		tallas = new JComboBox<Talla>();
+		
+		buscar = new JButton("Buscar");
+		anyadir = new JButton("Añadir");
+		
+		
+		
+		arriba.add(cliente);
+		
+		centro_izda.add(tipo);
+		centro_izda.add(tipos);
+		centro_izda.add(color);
+		centro_izda.add(c);
+		centro_izda.add(precio);
+		centro_izda.add(preciobarra);
+		centro_izda.add(talla);
+		centro_izda.add(tallas);
+		centro_dcha.add(buscar);
+		centro.add(centro_izda);
+		centro.add(centro_dcha);
+		
+		abajo.add(informacion);
+		abajo.add(anyadir);
+		
+	}
+	
+	public static void main(String[] args) {
+		Ventana_Cliente vc =new Ventana_Cliente();
+		vc.setVisible(true);
+	}
+	
+
+}
