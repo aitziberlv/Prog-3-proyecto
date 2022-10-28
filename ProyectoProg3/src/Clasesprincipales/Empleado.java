bca15aff873b66f120ac121241301ae2d0a41b0e
aiitz <aiitz@10.164.60.244>
2022-10-28 10:09:16 +0200

    clasr empleado

--- ProyectoProg3/src/Clasesprincipales/Empleado.java
+++ ProyectoProg3/src/Clasesprincipales/Empleado.java
@@ -1,8 +1,27 @@
 package Clasesprincipales;
 
 
-public class Empleado {
+public class Empleado extends Persona{
 	private double salario;
+	
+	public Empleado(String nombre, String dni, String fechNa, double salario) {
+		super(nombre, dni, fechNa);
+		this.salario = salario;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public int getEdad() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 
