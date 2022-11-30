c46023901f0fe3f12fa3198c4f1843523f90cf16
aiitz <aiitz@10.164.51.227>
2022-11-30 12:03:06 +0100

    ventana recursividad

---
+++ ProyectoProg3/src/Ventanas/Ventana_productosdisponibles.java
@@ -0,0 +1,34 @@
+package Ventanas;
+
+import java.awt.HeadlessException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import Clasesprincipales.Pedidos;
+//en esta ventana vamos a lanzar un hilo mostrando todos los pedidos
+import Clasesprincipales.Producto;
+
+
+public class Ventana_productosdisponibles extends JFrame{
+	private ArrayList<Pedidos> p=new ArrayList<>();
+
+	public Ventana_productosdisponibles(ArrayList<Pedidos> p) throws HeadlessException {
+		super();
+		this.p = p;
+	}
+	private JPanel panelprincipal ;
+	public void inizializarventana() {
+		panelprincipal=new JPanel();
+		//Aqui es donde pondremos el hilo: 
+		for (Pedidos prod:p) {
+			for(Producto prod2:prod.getLista_pedidos()) {
+				
+			}
+		}
+		
+		
+		this.add(panelprincipal);
+	}
+}
